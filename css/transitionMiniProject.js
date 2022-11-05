3d5e5974f87b7ddf8429221640538f81e1c85651
Kakaire Steven King <kakairestevenking@gmail.com>
2022-11-05 15:00:46 +0300

    Responsive

---
+++ css/transitionMiniProject.js
@@ -0,0 +1,17 @@
+transitionMiniProject.js;
+'use strict'
+let tileLoad = function () {
+    // Variables
+    var container = document.getElementsByClassName('container')[0];
+    var tule = document.getElementsByClassName('tile')[0];
+    var containerWidth = container.scrollWidth;
+    var tileWidth = tile.scrollWidth;
+    container.classList.append('complete');
+    for (let i = 0; i < fullWidth; i ++) {
+        
+    }
+
+    
+
+
+};
